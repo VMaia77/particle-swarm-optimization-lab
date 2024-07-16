00af5b7180240c6b24f0c72f7ae6036c2cb19e01
VMaia77 <vinicius.a.maia77@gmail.com>
2024-07-15 21:11:27 -0300

    Change from inheritance to composition

--- src/particle_swarm.py
+++ src/particle_swarm.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from src.particle import Particle
 
 
-class ParticleSwarmOptimizer(Particle):
+class ParticleSwarmOptimizer:
 
     def __init__(self, objective: callable, sense: str, n_dimensions: int, min_x: float, max_x: float, n_particles: int = 20, 
                  n_iterations: int = 50, w: float = 0.75, c1: float = 0.8, c2: float = 0.9, constraints: list[callable] = []) -> None:
@@ -22,12 +22,12 @@ class ParticleSwarmOptimizer(Particle):
 
         self.constraints = constraints
 
-        self.particles = [Particle(self.get_n_dimensions(), self.get_min_x(), self.get_max_x()) for _ in range(n_particles)]
+        self.particles = [Particle(self.n_dimensions, self.min_x, self.max_x) for _ in range(n_particles)]
         
         self.best_objective_value = 1e21 if self.get_sense() == 0 else -1e21
 
         for particle in self.get_particles():
-            particle.set_best_objective_value(self.get_best_objective_value())
+            particle.set_best_objective_value(self.best_objective_value)
 
         self.best_position = self.particles[0].get_position()
 
@@ -46,7 +46,7 @@ class ParticleSwarmOptimizer(Particle):
         return self.objective_values
 
     def add_objective_value(self) -> None:
-        self.objective_values += self.get_best_objective_value(),
+        self.objective_values += self.best_objective_value,
 
     def evalute_solution(self, solution: np.array) -> float:
         return self.objective(solution)
@@ -67,10 +67,10 @@ class ParticleSwarmOptimizer(Particle):
 
             if self.sense == 0:
                 condition_local = particle_objective_value < particle.get_best_objective_value()
-                condition_global = particle_objective_value < self.get_best_objective_value()
+                condition_global = particle_objective_value < self.best_objective_value
             else:
                 condition_local = particle_objective_value > particle.get_best_objective_value()
-                condition_global = particle_objective_value > self.get_best_objective_value()
+                condition_global = particle_objective_value > self.best_objective_value
 
             if not self.is_feasible(current_position):
                 continue
@@ -80,8 +80,8 @@ class ParticleSwarmOptimizer(Particle):
                 particle.set_best_objective_value(particle_objective_value)
 
             if condition_global:
-                self.set_best_position(current_position)
-                self.set_best_objective_value(particle_objective_value)
+                self.best_position = current_position
+                self.best_objective_value = particle_objective_value
                 
     def move_particle(self, particle: Particle) -> None:
 
@@ -89,7 +89,7 @@ class ParticleSwarmOptimizer(Particle):
 
         updated_velocity = self.w * particle.get_velocity() + \
             self.c1 * r1 * (particle.get_best_position() - particle.get_position()) + \
-                        self.c2 * r2 * (self.get_best_position() - particle.get_position())
+                        self.c2 * r2 * (self.best_position - particle.get_position())
 
         particle.set_velocity(updated_velocity)
         particle.move_particle()
@@ -109,7 +109,7 @@ class ParticleSwarmOptimizer(Particle):
             self.update_best_solutions()
             self.add_objective_value()
     
-        if not self.is_feasible(self.get_best_position()):
+        if not self.is_feasible(self.best_position):
             print("No feasible solution found.")
             self.best_position = np.array([np.nan, np.nan])
 
